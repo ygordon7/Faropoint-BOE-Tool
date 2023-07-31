a6481005560793da311c8fcb286e80b1b88f8991
ygordon7 <140986880+ygordon7@users.noreply.github.com>
2023-07-30 20:26:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-document.getElementById('calculateButton').addEventListener('click', function() {
-    // Retrieve inputs
-    let purchasePrice = parseFloat(document.getElementById('purchasePrice').value);
-    let sf = parseFloat(document.getElementById('sf').value);
-    let inPlaceRent = parseFloat(document.getElementById('inPlaceRent').value);
-    let trendedMarketRentGrowth = parseFloat(document.getElementById('trendedMarketRentGrowth').value);
-    let marketRent = parseFloat(document.getElementById('marketRent').value);
-    
-    // Verify the inputs
-    if (isNaN(purchasePrice) || isNaN(sf) || isNaN(inPlaceRent) || isNaN(trendedMarketRentGrowth) || isNaN(marketRent)) {
-        alert('Please ensure all fields are filled with valid numeric values.');
-        return;
+document.getElementById("calculate").addEventListener("click", function() {
+    // Retrieve input values
+    const purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
+    const sf = parseFloat(document.getElementById("sf").value);
+    const cost = parseFloat(document.getElementById("cost").value);
+    const inPlaceRent = parseFloat(document.getElementById("inPlaceRent").value);
+    const marketRent = parseFloat(document.getElementById("marketRent").value);
+    const investmentHorizon = parseFloat(document.getElementById("investmentHorizon").value);
+    const rentGrowth = parseFloat(document.getElementById("rentGrowth").value) / 100;
+
+    // Calculate derived values
+    const pricePerSF = purchasePrice / sf;
+    const totalCostPerSF = pricePerSF + cost;
+    const inPlaceNOI = inPlaceRent * sf;
+    const marketNOI = marketRent * sf;
+    const inPlaceCapRate = inPlaceNOI / purchasePrice;
+    const marketCapRate = marketNOI / purchasePrice;
+    const inPlaceYOC = inPlaceNOI / (sf * totalCostPerSF);
+    const marketYOC = marketNOI / (sf * totalCostPerSF);
+
+    let trendedMarketRent = marketRent;
+    for (let i = 0; i < investmentHorizon; i++) {
+        trendedMarketRent *= (1 + rentGrowth);
     }
+    const trendedMarketNOI = trendedMarketRent * sf;
+    const trendedMarketCapRate = trendedMarketNOI / purchasePrice;
+    const trendedMarketYOC = trendedMarketNOI / (sf * totalCostPerSF);
 
-    // Calculations
-    let pricePSF = purchasePrice / sf;
-    let inPlaceNOI = inPlaceRent * sf;
-    let inPlaceCapRate = inPlaceNOI / purchasePrice;
-    let marketNOI = marketRent * sf;
-    let marketCapRate = marketNOI / purchasePrice;
-    let trendedMarketRent = marketRent * (1 + trendedMarketRentGrowth / 100);
-    let trendedMarketNOI = trendedMarketRent * sf;
-    let trendedMarketCapRate = trendedMarketNOI / purchasePrice;
-
-    // Display the results
-    document.getElementById('pricePSF').textContent = `Price PSF: $${pricePSF.toFixed(2)}`;
-    document.getElementById('inPlaceCapRate').textContent = `In-place Cap Rate: ${(inPlaceCapRate * 100).toFixed(2)}%`;
-    document.getElementById('marketCapRate').textContent = `Market Cap Rate: ${(marketCapRate * 100).toFixed(2)}%`;
-    document.getElementById('trendedMarketRent').textContent = `Trended Market Rent: $${trendedMarketRent.toFixed(2)}`;
-    document.getElementById('trendedMarketCapRate').textContent = `Trended Market Cap Rate: ${(trendedMarketCapRate * 100).toFixed(2)}%`;
+    // Display results
+    document.getElementById("pricePerSFResult").innerText = pricePerSF.toFixed(2);
+    document.getElementById("totalCostPerSFResult").innerText = totalCostPerSF.toFixed(2);
+    document.getElementById("inPlaceCapRateResult").innerText = (inPlaceCapRate * 100).toFixed(2) + "%";
+    document.getElementById("marketCapRateResult").innerText = (marketCapRate * 100).toFixed(2) + "%";
+    document.getElementById("inPlaceYOCResult").innerText = (inPlaceYOC * 100).toFixed(2) + "%";
+    document.getElementById("marketYOCResult").innerText = (marketYOC * 100).toFixed(2) + "%";
+    document.getElementById("trendedMarketRentResult").innerText = trendedMarketRent.toFixed(2);
+    document.getElementById("trendedMarketCapRateResult").innerText = (trendedMarketCapRate * 100).toFixed(2) + "%";
+    document.getElementById("trendedMarketYOCResult").innerText = (trendedMarketYOC * 100).toFixed(2) + "%";
 });
