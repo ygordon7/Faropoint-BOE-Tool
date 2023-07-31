fc8cc8f2c73d7778696844f01254af797e511ae1
ygordon7 <140986880+ygordon7@users.noreply.github.com>
2023-07-30 21:12:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function() {
+
+    // Button event listener
     document.getElementById("calculate").addEventListener("click", function() {
-        let purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
-        let squareFeet = parseFloat(document.getElementById("squareFeet").value);
+
+        // Getting values from inputs
+        let purchasePrice = parseFloat(document.getElementById("purchasePrice").value.replace(/,/g, ''));
+        let squareFeet = parseFloat(document.getElementById("squareFeet").value.replace(/,/g, ''));
         let inPlaceRent = parseFloat(document.getElementById("inPlaceRent").value);
         let costToStabilize = parseFloat(document.getElementById("costToStabilize").value);
         let marketRent = parseFloat(document.getElementById("marketRent").value);
         let investmentHorizon = parseFloat(document.getElementById("investmentHorizon").value);
         let rentGrowth = parseFloat(document.getElementById("rentGrowth").value) / 100;
-
+        
+        // Calculations
         let pricePerSF = purchasePrice / squareFeet;
         let inPlaceNOI = inPlaceRent * squareFeet;
         let inPlaceCapRate = (inPlaceNOI / purchasePrice) * 100;
         let marketNOI = marketRent * squareFeet;
         let marketCapRate = (marketNOI / purchasePrice) * 100;
-
         let trendedMarketRent = marketRent;
 
         for (let i = 0; i < investmentHorizon; i++) {
@@ -23,23 +28,22 @@ document.addEventListener("DOMContentLoaded", function() {
         let totalCostPerSF = pricePerSF + costToStabilize;
         let trendedMarketCapRate = (trendedMarketRent * squareFeet / purchasePrice) * 100;
         let trendedMarketYOC = (trendedMarketRent * squareFeet / (totalCostPerSF * squareFeet)) * 100;
-
-        document.getElementById("pricePerSF").textContent = pricePerSF.toFixed(2);
-        document.getElementById("inPlaceNOI").textContent = formatCurrency(inPlaceNOI);
+        
+        // Outputting results to HTML
+        document.getElementById("summaryPurchasePrice").textContent = '$' + purchasePrice.toLocaleString();
+        document.getElementById("summarySquareFeet").textContent = squareFeet.toLocaleString();
+        document.getElementById("pricePerSF").textContent = '$' + pricePerSF.toFixed(2);
+        document.getElementById("inPlaceNOI").textContent = '$' + inPlaceNOI.toFixed(2);
         document.getElementById("inPlaceCapRate").textContent = inPlaceCapRate.toFixed(2) + "%";
-        document.getElementById("marketNOI").textContent = formatCurrency(marketNOI);
+        document.getElementById("marketNOI").textContent = '$' + marketNOI.toFixed(2);
         document.getElementById("marketCapRate").textContent = marketCapRate.toFixed(2) + "%";
-        document.getElementById("trendedMarketRent").textContent = formatCurrency(trendedMarketRent);
-        document.getElementById("totalCostPerSF").textContent = totalCostPerSF.toFixed(2);
+        document.getElementById("trendedMarketRent").textContent = '$' + trendedMarketRent.toFixed(2);
+        document.getElementById("totalCostPerSF").textContent = '$' + totalCostPerSF.toFixed(2);
         document.getElementById("trendedMarketCapRate").textContent = trendedMarketCapRate.toFixed(2) + "%";
         document.getElementById("trendedMarketYOC").textContent = trendedMarketYOC.toFixed(2) + "%";
 
-        document.getElementById("summaryPurchasePrice").textContent = formatCurrency(purchasePrice);
-        document.getElementById("summarySquareFeet").textContent = squareFeet.toLocaleString();
+        // Show the output section
         document.getElementById("outputSection").style.display = "block";
+
     });
 });
-
-function formatCurrency(value) {
-    return "$" + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-}
