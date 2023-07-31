449837d2504f741ba6c835faf613ceb05a30c044
ygordon7 <140986880+ygordon7@users.noreply.github.com>
2023-07-31 08:42:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function formatCurrency(inputElement) {
-    let value = parseFloat(inputElement.value);
+    let value = parseFloat(inputElement.value.replace(/[\$,]/g, ''));
     if (!isNaN(value)) {
-        inputElement.value = `$${value.toFixed(2)}`;
+        inputElement.value = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
     } else {
         inputElement.value = "";
     }
 }
 
 function formatPercentage(inputElement) {
-    let value = parseFloat(inputElement.value);
+    let value = parseFloat(inputElement.value.replace('%', ''));
     if (!isNaN(value)) {
         inputElement.value = `${value.toFixed(1)}%`;
     } else {
@@ -17,7 +17,7 @@ function formatPercentage(inputElement) {
 }
 
 function formatYears(inputElement) {
-    let value = parseInt(inputElement.value, 10);
+    let value = parseInt(inputElement.value.replace(' Years', ''));
     if (!isNaN(value)) {
         inputElement.value = `${value} Years`;
     } else {
@@ -26,37 +26,51 @@ function formatYears(inputElement) {
 }
 
 function calculateOutputs() {
-    // Grab input values
-    const rsf = parseFloat(document.getElementById("rsf").value);
-    const price = parseFloat(document.getElementById("price").value);
-    const inPlaceNOI = parseFloat(document.getElementById("inPlaceNOI").value.replace('$', ''));
-    const marketRent = parseFloat(document.getElementById("marketRent").value.replace('$', ''));
-    const costToStabilize = parseFloat(document.getElementById("costToStabilize").value.replace('$', ''));
-    const growthCAGR = parseFloat(document.getElementById("growthCAGR").value.replace('%', '')) / 100;
-    const investmentHorizon = parseInt(document.getElementById("investmentHorizon").value);
+    const RSF = parseFloat(document.getElementById('RSF').value);
+    const price = parseFloat(document.getElementById('price').value.replace(/[\$,]/g, ''));
+    const inPlaceNOI = parseFloat(document.getElementById('inPlaceNOI').value.replace(/[\$,]/g, ''));
+    const marketRent = parseFloat(document.getElementById('marketRent').value.replace(/[\$,]/g, ''));
+    const costToStabilize = parseFloat(document.getElementById('costToStabilize').value.replace(/[\$,]/g, ''));
+    const growthCAGR = parseFloat(document.getElementById('growthCAGR').value.replace('%', '')) / 100;
+    const investmentHorizon = parseInt(document.getElementById('investmentHorizon').value.replace(' Years', ''));
 
-    // Perform calculations
-    const pricePSF = price / rsf;
+    const pricePSF = price / RSF;
     const totalCostPSF = pricePSF + costToStabilize;
     const inPlaceCapRate = (inPlaceNOI / pricePSF) * 100;
-    const marketCapRate = (marketRent / pricePSF) * 100;
+    const marketCapRate = (marketRent / RSF) * 100;
     const marketYOC = marketRent / totalCostPSF;
     const trendedMarket = marketRent * Math.pow((1 + growthCAGR), investmentHorizon);
     const trendedMarketCap = trendedMarket / pricePSF;
     const trendedMarketYOC = trendedMarket / totalCostPSF;
 
-    // Display results in output fields
-    document.getElementById("outputRSF").textContent = `${rsf.toLocaleString(undefined, {maximumFractionDigits: 2})}`;
-    document.getElementById("outputPrice").textContent = `$${price.toLocaleString(undefined, {maximumFractionDigits: 2})}`;
-    document.getElementById("outputPricePSF").textContent = `$${pricePSF.toFixed(2)}`;
-    document.getElementById("outputCostToStabilize").textContent = `$${costToStabilize.toFixed(2)}`;
-    document.getElementById("outputTotalCostPSF").textContent = `$${totalCostPSF.toFixed(2)}`;
-    document.getElementById("outputInPlaceNOIPSF").textContent = `$${inPlaceNOI.toFixed(2)}`;
-    document.getElementById("outputInPlaceCapRate").textContent = `${inPlaceCapRate.toFixed(2)}%`;
-    document.getElementById("outputMarketRentPSF").textContent = `$${marketRent.toFixed(2)}`;
-    document.getElementById("outputMarketCapRate").textContent = `${marketCapRate.toFixed(2)}%`;
-    document.getElementById("outputMarketYOC").textContent = `$${marketYOC.toFixed(2)}`;
-    document.getElementById("outputTrendedMarket").textContent = `$${trendedMarket.toFixed(2)}`;
-    document.getElementById("outputTrendedMarketCap").textContent = `${trendedMarketCap.toFixed(2)}%`;
-    document.getElementById("outputTrendedMarketYOC").textContent = `$${trendedMarketYOC.toFixed(2)}`;
+    const outputDiv = document.querySelector('.output-section');
+    outputDiv.innerHTML = `
+        RSF: ${RSF.toLocaleString()}
+        <br>
+        Price: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price)}
+        <br>
+        Price PSF: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(pricePSF)}
+        <br>
+        Cost to Stabilize: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(costToStabilize)}
+        <br>
+        Total Cost PSF: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(totalCostPSF)}
+        <br>
+        In Place NOI PSF: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(inPlaceNOI)}
+        <br>
+        In Place Cap Rate: ${inPlaceCapRate.toFixed(2)}%
+        <br>
+        Market Rent PSF: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(marketRent)}
+        <br>
+        Market Cap Rate: ${marketCapRate.toFixed(2)}%
+        <br>
+        Market Yield on Cost: ${marketYOC.toFixed(2)}
+        <br>
+        Trended Market: ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(trendedMarket)}
+        <br>
+        Trended Market Cap: ${trendedMarketCap.toFixed(2)}
+        <br>
+        Trended Market YOC: ${trendedMarketYOC.toFixed(2)}
+    `;
 }
+
+document.getElementById('runBtn').addEventListener('click', calculateOutputs);
